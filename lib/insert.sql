099d0d4ad7b14f903265406b3f6d75153b072257
Brian Simonton <bsimonton78@gmail.com>
2019-03-28 20:49:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "Brown", "Cuddly", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Brown", "Crazy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabita", 8, "F", "Grizzly", "Kind", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Brown", "Angry", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "Black", "Loud", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "M", "Grizzly", "Loving", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Black", "Sluggish", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 16, "M", "Grizzly", "Hungry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "Brown", "Sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 6, "F", "Grizzly", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabita", 3, "F","Black", "Nervous", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "Grizzly", "Depressed", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 7, "F", "Black", "Weird", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 99, "M", "Black", "Paranoid", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 56, "F", "Black", "Crazy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, "M", "Grizzly", "Mean", 1);
